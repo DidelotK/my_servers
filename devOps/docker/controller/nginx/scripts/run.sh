9e704e7383abd689b0a00f78b42dad6b6f27cb6f
DidelotK <didelotkev@gmail.com>
2017-12-25 21:06:00 +0100

    [EVOL] Change email for let's encrypt account

--- devOps/docker/controller/nginx/scripts/run.sh
+++ devOps/docker/controller/nginx/scripts/run.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 # Configure HTTPS with Certbox
-certbot --nginx --non-interactive --agree-tos --redirect -m didelotkev@gmail.com -d cv.didelotkev.ovh -d jenkins.didelotkev.ovh
+certbot --nginx --non-interactive --agree-tos --redirect -m work@didelotkev.ovh -d cv.didelotkev.ovh -d jenkins.didelotkev.ovh
 
 # Automating renewal HTTPS certificates
 certbot renew --dry-run
