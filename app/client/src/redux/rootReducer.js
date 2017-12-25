92117965863a50fa8ab8c925337d53a81fbb3152
DidelotK <didelotkev@gmail.com>
2017-12-25 01:22:29 +0100

    [REMOVE] Remove react app (moved in other repo)

--- app/client/src/redux/rootReducer.js
+++
@@ -1,9 +0,0 @@
-import { combineReducers } from 'redux';
-
-import appReducer from './appRedux/appReducer';
-
-const rootReducer = combineReducers({
-  appReducer
-});
-
-export default rootReducer;
--- app/server/src/constants/appConstants.js
+++
@@ -1,9 +0,0 @@
-export default {
-  SERVER_PORT: 4000,
-  SERVER_IP: 'http://localhost:4000',
-  MONGO: 'mongodb://db/omniscol-dev',
-  SECRET: 'secret-dev',
-  LOG_LEVEL: 'silly',
-  TOKEN_SECRET: 'token-dev',
-  TOKEN_EXPIRES: '120s'
-};
