6001239a417ea96a3cbd37cf29a7cfa304292d76
DidelotK <didelotkev@gmail.com>
2017-12-28 16:42:05 +0100

    [ADD] Add grafana and portainer services (TODO fix nginx jenkins.conf configs: https not handled correctly with the new fix)

--- devOps/docker/controller/nginx/scripts/run.sh
+++ devOps/docker/controller/nginx/scripts/run.sh
@@ -4,7 +4,7 @@
 nginx
 
 # Configure HTTPS with Certbox
-certbot --nginx --non-interactive --agree-tos --redirect -m work@didelotkev.ovh -d cv.didelotkev.ovh -d jenkins.didelotkev.ovh -d portainer.didelotkev.ovh
+certbot --nginx --non-interactive --agree-tos --redirect -m work@didelotkev.ovh -d cv.didelotkev.ovh -d jenkins.didelotkev.ovh -d portainer.didelotkev.ovh -d grafana.didelotkev.ovh
 
 # Automating renewal HTTPS certificates
 certbot renew --dry-run
