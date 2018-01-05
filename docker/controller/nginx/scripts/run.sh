ee7ee4e685cea7258b31f6a41e26c969d4b3eb01
DidelotK <didelotkev@gmail.com>
2018-01-05 11:16:20 +0100

    [EVOL] Ask https certificates only if they didn't exist

--- docker/controller/nginx/scripts/run.sh
+++ docker/controller/nginx/scripts/run.sh
@@ -1,15 +1,17 @@
 #!/usr/bin/env bash
 
 # Launch nginx (on background)
-# nginx
+nginx
 
-# Configure HTTPS with Certbox
-# certbot --nginx --non-interactive --agree-tos --redirect -m work@didelotkev.ovh -d cv.didelotkev.ovh -d jenkins.didelotkev.ovh -d portainer.didelotkev.ovh -d grafana.didelotkev.ovh
-# certbot certonly --standalone --agree-tos --non-interactive -m work@didelotkev.ovh -d test@didelotkev.ovh
+# Only ask for certificates if they did not exist
+if [ -d "/etc/letsencrypt/live" ]; then
+    certbot --nginx --non-interactive --agree-tos --redirect -m work@didelotkev.ovh -d cv.didelotkev.ovh -d jenkins.didelotkev.ovh -d portainer.didelotkev.ovh -d grafana.didelotkev.ovh
+    #certbot certonly --standalone --agree-tos --non-interactive -m work@didelotkev.ovh -d test@didelotkev.ovh
+fi
 
 # Automating renewal HTTPS certificates
-# certbot renew --dry-run
+certbot renew --dry-run
 
-# nginx -s quit
+nginx -s quit
 
 nginx -g "daemon off;"
