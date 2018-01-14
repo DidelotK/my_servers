914d2c646a908663257a7301e4a2d153e9a79b88
DidelotK <didelotkev@gmail.com>
2018-01-14 12:49:30 +0100

    [EVOL] Clean nginx conf (again ahah)

--- docker/controller/nginx/scripts/run.sh
+++ docker/controller/nginx/scripts/run.sh
@@ -4,14 +4,15 @@
 nginx
 
 # Only ask for certificates if they did not exist
-if [ -d "/etc/letsencrypt/live" ]; then
+if [ ! -d "/etc/letsencrypt/live" ]; then
+    echo "CERTBOT: GET CERTFICATES"
     certbot --nginx --non-interactive --agree-tos --redirect -m work@didelotkev.ovh -d cv.didelotkev.ovh -d jenkins.didelotkev.ovh -d portainer.didelotkev.ovh -d grafana.didelotkev.ovh
-    #certbot certonly --standalone --agree-tos --non-interactive -m work@didelotkev.ovh -d test@didelotkev.ovh
 fi
 
 # Automating renewal HTTPS certificates
-certbot renew --dry-run
+certbot renew
 
 nginx -s quit
 
+echo "NGING: START"
 nginx -g "daemon off;"
